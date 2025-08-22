e0f8fb1094a18d1416f5ca9ef33eb853b31e133a
Kathryn <137423893+kathrynpotten@users.noreply.github.com>
2025-08-22 15:34:35 +0100

    update settings

--- src/dutchvocab/settings.py
+++ src/dutchvocab/settings.py
@@ -1,23 +1,67 @@
 import inquirer
-
-print("Welcome to Settings")
-
-# default settings
-report_path = ""  # save to working folder
-
-options = ["Report Save Folder", "Plots Save Folder"]
-select_setting = [
-    inquirer.List("setting", message="Select setting to edit", choices=options)
-]
-selected = inquirer.prompt(select_setting)
-print("Going to Report Save Folder setting...\n")
+import os
 
 
-if selected["setting"] == "Report Save Folder":
-    report_path = input("Insert path for saving reports: ")
+def main():
 
+    print("Welcome to Settings")
 
-with open("settings.txt", "w") as file:
-    file.write(report_path)
+    # default settings if not preset
+    report_path = ""  # save to working Path
+    output = "Yes"  # output reports after each session
 
-print("\nYour settings have been saved.")
+    if os.path.isfile("settings.txt"):
+        with open("settings.txt", "r") as file:
+            settings = file.read().splitlines()
+        report_path = settings[0].rstrip("/")
+        output = settings[1]
+
+    print(
+        f"\nCurrent settings:\nReport Save Path      {report_path}\nReport Output       {output}"
+    )
+
+    edit_settings = input("\nDo you want to edit any settings? (Y/N) ")
+    if edit_settings.upper() != "Y":
+        setting = False
+    else:
+        setting = True
+
+    while setting == True:
+
+        options = ["Report Save Path", "Plots Save Path", "Report Output"]
+        select_setting = [
+            inquirer.List("setting", message="Select setting to edit", choices=options)
+        ]
+        selected = inquirer.prompt(select_setting)
+        print(f"Going to {selected['setting']} setting...\n")
+
+        if selected["setting"] == "Report Save Path":
+            report_path = input("Insert path for saving reports: ")
+            print(f"\nReports will be saved in {report_path.rstrip('/')}/Reports")
+
+        elif selected["setting"] == "Report Output":
+            select_output = [
+                inquirer.List(
+                    "setting",
+                    message="Do you want to output reports at the end of each session?",
+                    choices=["Yes", "No"],
+                )
+            ]
+            output = inquirer.prompt(select_output)
+            output = output["setting"]
+            if output == "Yes":
+                print("Reports will be output at the end of each session.")
+            elif output == "No":
+                print("Reports will not be output at the end of each session.")
+
+        continue_setting = input("\nChange other settings? (Y/N) ")
+        if continue_setting.upper() != "Y":
+            setting = False
+            break
+
+    with open("settings.txt", "w") as file:
+        file.write(report_path)
+        file.write("\n")
+        file.write(output)
+
+    print("\nYour settings have been saved.")
