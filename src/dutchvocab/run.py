f73f1fdfaa1a638f926e77f826f78fc2bffe2c58
Kathryn <137423893+kathrynpotten@users.noreply.github.com>
2025-08-22 15:16:20 +0100

    add settings for report output

--- src/dutchvocab/run.py
+++ src/dutchvocab/run.py
@@ -11,6 +11,12 @@ import os
 
 def main():
 
+    # get settings
+    with open("settings.txt", "r") as file:
+        settings = file.read().splitlines()
+    report_path = settings[0].rstrip("/")
+    report_output = settings[1]
+
     playing = True
     log = pd.DataFrame(columns=["Module", "Lesson", "Questions", "Score"])
     while playing:
@@ -117,20 +123,24 @@ def main():
             "learning_log.csv", mode="a", header=True, index=True, index_label="Date"
         )
     vf.visualisation_today()
+
     # generate weekly/monthly report, ask about progress report
-    # print("\nUpdating reports...")
-    # log_full = pd.read_csv("learning_log.csv")
-    # weekly_pdf = pdf_constructor.build_pdf(log_full, "Weekly")
-    # weekly_pdf.output(
-    #     f"Reports/Weekly_Reports/{(date.today() - timedelta(days=date.today().weekday())).strftime('%Y%m%d')}_Report.pdf",
-    #     "F",
-    # )
-    # monthly_pdf = pdf_constructor.build_pdf(log_full, "Monthly")
-    # monthly_pdf.output(
-    #     f"Reports/Monthly_Reports/{date.today().strftime('%Y_%B')}_Report.pdf",
-    #     "F",
-    # )
-    # print("\nWeekly and monthly reports updated.")
+    if report_output == "Yes":
+        print("\nUpdating reports...")
+        log_full = pd.read_csv("learning_log.csv")
+        weekly_pdf = pdf_constructor.build_pdf(log_full, "Weekly")
+
+        weekly_pdf.output(
+            f"{report_path}/Reports/Weekly_Reports/{(date.today() - timedelta(days=date.today().weekday())).strftime('%Y%m%d')}_Report.pdf",
+            "F",
+        )
+        monthly_pdf = pdf_constructor.build_pdf(log_full, "Monthly")
+        monthly_pdf.output(
+            f"{report_path}/Reports/Monthly_Reports/{date.today().strftime('%Y_%B')}_Report.pdf",
+            "F",
+        )
+
+        print("\nWeekly and monthly reports updated.")
     # progress = input("Would you like to generate a progress report?  (Y/N)       ")
     # if progress == "Y":
     #     pdf = pdf_constructor.build_pdf(log_full, "Progress")
